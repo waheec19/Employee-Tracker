e1763a527024923018191b1e73bc8446045967b0
waheec19 <waheechowdhury80@gmail.com>
2022-12-08 21:43:58 -0600

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'Wc236744#',
+    //enter password below
+    password: '',
     database: "employee_managerDB"
 });
 
